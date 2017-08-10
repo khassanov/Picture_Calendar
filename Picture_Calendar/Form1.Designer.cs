93d56222cb47477b44afb4488cb005a3bcfe76c9
NURLAN KHASSANOV <NURLAN KHASSANOV>
2017-08-10 12:39:33 +0600

    Add project files.

---
+++ Picture_Calendar/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Picture_Calendar
+{
+    partial class Pictures_Calendar_Beta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Pictures_Calendar_Beta));
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(0, 0);
+            this.monthCalendar1.MaxSelectionCount = 1;
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 2;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(659, 383);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // Pictures_Calendar_Beta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 383);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Pictures_Calendar_Beta";
+            this.Text = "Picture_Calendar";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Picture_Calendar/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Picture_Calendar
+
+{
+    public partial class Pictures_Calendar_Beta : Form
+    {
+        
+        public Pictures_Calendar_Beta()
+        {
+            InitializeComponent();
+            show_picture();
+        }
+
+        public void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            show_picture();
+        }
+            private void show_picture() { 
+            string month = monthCalendar1.SelectionStart.Month.ToString();
+            int intmonth = int.Parse(month);
+            
+            switch (intmonth)
+            {
+                case 1:
+                    pictureBox1.Image = Image.FromFile("12months\\January.jpg");
+                    break;
+                case 2:
+                    pictureBox1.Image = Image.FromFile("12months\\February.jpg");
+                    break;
+                case 3:
+                    pictureBox1.Image = Image.FromFile("12months\\March.jpg");
+                    break;
+                case 4:
+                    pictureBox1.Image = Image.FromFile("12months\\April.jpg");
+                    break;
+                case 5:
+                    pictureBox1.Image = Image.FromFile("12months\\May.jpg");
+                    break;
+                case 6:
+                    pictureBox1.Image = Image.FromFile("12months\\June.jpg");
+                    break;
+                case 7:
+                    pictureBox1.Image = Image.FromFile("12months\\July.jpg");
+                    break;
+                case 8:
+                    pictureBox1.Image = Image.FromFile("12months\\August.jpg");
+                    break;
+                case 9:
+                    pictureBox1.Image = Image.FromFile("12months\\September.jpg");
+                    break;
+                case 10:
+                    pictureBox1.Image = Image.FromFile("12months\\October.jpg");
+                    break;
+                case 11:
+                    pictureBox1.Image = Image.FromFile("12months\\November.jpg");
+                    break;
+                case 12:
+                    pictureBox1.Image = Image.FromFile("12months\\December.jpg");
+                    break;
+            }
+
+
+        }
+
+      
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+            monthCalendar1.Visible = !monthCalendar1.Visible;
+        }
+    }
+}
+
